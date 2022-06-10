b5430da9d9d49f4a3beaf0a3122288bfdc0286bd
Jusong Yu <jusong.yeu@gmail.com>
2022-06-10 12:27:51 +0200

    Link all lanthanides

--- pre_and_post_process/link_pseudos.py
+++ pre_and_post_process/link_pseudos.py
@@ -8,12 +8,12 @@ def main():
     element = sys.argv[1]
     func = sys.argv[2]  # functional
     
-    func_path = os.path.join(f'../libraries-{func}')
-    dst_path = os.path.join(f'../_sssp_{func}', element)
+    func_path = os.path.join(f'./libraries-{func}')
+    link_dst_path = os.path.join(f'./_sssp_{func}', element)
     
     # create element folder if not exist
-    if not os.path.exists(dst_path):
-        os.mkdir(dst_path)
+    if not os.path.exists(os.path.join(f'./_sssp_{func}', element)):
+        os.mkdir(os.path.join(f'./_sssp_{func}', element))
     
     for folder in [
         "NC-DOJOv4-standard",
@@ -29,21 +29,27 @@ def main():
         "US-PSL1.0.0-high",
         "US-PSL1.0.0-low",
         "UNCATOGRIZED",
-        "PAW-RE-Wentzcovitch",
+        "PAW-RE-Wentzcovitch/legacy",
+        "PAW-RE-Wentzcovitch/neo",
     ]:
         psp_folder = os.path.join(func_path, folder)
         for psp_filename in os.listdir(psp_folder):
             if psp_filename.startswith(f'{element}.'):
                 src = os.path.join('..', '..', psp_folder, psp_filename)
-                dst = os.path.join(dst_path, psp_filename)
-                print(f'Linking: {src} -> {dst}')
+                dst = os.path.join(link_dst_path, psp_filename)
                 
                 try:
                     os.symlink(src, dst)
                 except FileExistsError:
                     os.remove(dst)
                     os.symlink(src, dst)
+                    print(f'Exist: Clean and Linking: {src} -> {dst}')
                     continue
+                except FileNotFoundError:
+                    # for example no SG15 lanthanides
+                    continue
+                else:
+                    print(f'Linking: {src} -> {dst}')
                 
 if __name__ == '__main__':
     main()
